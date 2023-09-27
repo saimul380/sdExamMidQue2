ab988e6b31e12777a39709beeea2017f81a3e630
saimul380 <c223173@ugrad.iiuc.ac.bd>
2023-09-27 19:44:12 +0600

    Add project files.

---
+++ sdExamq2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace sdExamq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLine = Console.ReadLine();
+            string secondLine = Console.ReadLine();
+            int count=0;
+            int i;
+            for (i = 0; i < firstLine.Length; i++)
+            {
+                if (secondLine[0] == firstLine[i])
+                {
+                    count = i + 1;
+                    break;
+                }
+            }
+            if(count==0) {
+                Console.WriteLine("Does not contain the alphabet");
+            }
+            else
+            {
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
